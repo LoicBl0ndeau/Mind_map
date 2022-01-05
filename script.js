37e74cf2b172697d24df4c0de30c8f81170064f5
LoicBl0ndeau <loic.blondeau@student.junia.com>
2022-01-05 14:41:48 +0100

    bouton importe fichier .mm et fonction qui décode les caractères spéciaux et fonction qui décode le .mm

--- script.js
+++ script.js
@@ -1,37 +1,97 @@
 $(document).ready(function(){
-  $.get("test.mm", function(data, status){
-    var mm = [];
-    var text = false;
-    var id = 0;
-    var precedent = 0;
-    var ancienPrecedents = [];
-    for (var i = 0; i < data.length; i++) {
-      if(data[i] == "T" && data[i+1] == "E" && data[i+2] == "X" && data[i+3] == "T"){ //On regarde si on a une donnée à récupérer bientôt
-        text = true;
+  $('#fichier').on("change",function(){ //Quand on importe un fichier.
+    var fileReader = new FileReader();
+    $(fileReader).on("load",function(){ //On attend que le fichier soit intégralement chargé.
+      var data = fileReader.result;  // Il y a dans data les données du fichier encodées en base64
+      var trueData = "";
+      for (var i = 0; i < data.length; i++) { //On purifie les data pour enlever le potentiel texte du début qui explique l'encodage
+        if(data[i] == ","){
+          trueData = "";
+        }
+        else{
+          trueData += data[i];
+        }
       }
-      else if(data[i] == "/" && data[i+1] == "n" && data[i+2] == "o" && data[i+3] == "d" && data[i+4] == "e"){
-        precedent = ancienPrecedents[ancienPrecedents.length-1];
-        ancienPrecedents.pop();
-      }
-      else if(text === true && data[i] == '"'){ //On récupère la phrase
-        var j = i + 1;
-        var phrase = "";
-        while(data[j] != '"'){
+      trueData = atob(trueData); //atob permet de décoder la base64
+      recupData(trueData);
+    });
+    fileReader.readAsDataURL($('#fichier').prop('files')[0]);
+  });
+});
+
+function carac(i,data){ //fonction qui décode les caractères mal encodé en UTF-8 par FreeMind
+  var code ="";
+  var k=i+1;
+  var caractere ="";
+  var convert =[ //table de conversion
+      {cod:"amp",lettre:"&"},
+      {cod:"#xe9",lettre:"é"},
+      {cod:"quot",lettre:'"'},
+      {cod:"apos",lettre:"'"},
+      {cod:"#xe8",lettre:"è"},
+      {cod:"#xe7",lettre:"ç"},
+      {cod:"#xe0",lettre:"à"},
+      {cod:"#xf9",lettre:"ù"},
+      {cod:"lt",lettre:"<"},
+      {cod:"#xb0",lettre:"°"},
+      {cod:"#xa8",lettre:"¨"},
+      {cod:"#xa3",lettre:"£"},
+      {cod:"#xb5",lettre:"µ"},
+      {cod:"#xa7",lettre:"§"},
+      {cod:"#xa4",lettre:"¤"},
+      {cod:"gt",lettre:">"},
+      {cod:"#xb2",lettre:"²"}
+  ];
+  if (data[i]=="&"){ //On calcul de combien de caractères il faut avancer pour passer les symboles non reconnus.
+    while (data[k]!=";"){
+      code += data[k];
+      k++;
+    }
+  }
+  for (var i=0;i< convert.length;i++){
+    if (code==convert[i].cod){ //On cherche grâce à la table de conversion à quel lettre le caractère non reconnus correspond
+      caractere = convert[i].lettre;
+    }
+  }
+  return [caractere,k+1];
+}
+
+function recupData(data){ //fonction qui récupère dans la variable mm, le texte, le parent et la génération de l'enfant (degré) de chaque noeud. On leur associe également un identifiant (id) unique.
+  var mm = [];
+  var text = false;
+  var id = 0;
+  var precedent = 0;
+  var ancienPrecedents = []; //tableau qui sert à garder en mémoire le chemin à travers tous les parents effectuées pour arriver à l'élement actuel.
+  for (var i = 0; i < data.length; i++) { //On parcours chaque caractère du fichier .mm un à un.
+    if(data[i] == "T" && data[i+1] == "E" && data[i+2] == "X" && data[i+3] == "T"){ //On regarde si on a une donnée à récupérer bientôt
+      text = true;
+    }
+    else if(data[i] == "/" && data[i+1] == "n" && data[i+2] == "o" && data[i+3] == "d" && data[i+4] == "e"){ //Si on rencontre un node fermant (</node>), on supprimer le dernier élément du tableau ancienPrecedents.
+      precedent = ancienPrecedents[ancienPrecedents.length-1];
+      ancienPrecedents.pop();
+    }
+    else if(text === true && data[i] == '"'){ //On récupère la phrase
+      var j = i + 1;
+      var phrase = "";
+      while(data[j] != '"'){
+        if (data[j]=="&"){ //Si il y a un caractère incompris
+          var tab = carac(j,data);
+          phrase += tab[0];
+          j=tab[1];
+        }
+        else{
           phrase += data[j];
           j++;
         }
-        mm.push({text: phrase, id: id, precedent: precedent, degre: ancienPrecedents.length});
-        if(data[j+1] != "/"){
-          ancienPrecedents.push(precedent);
-          precedent = id;
-        }
-        id++;
-        text = false;
       }
+      mm.push({text: phrase, id: id, precedent: precedent, degre: ancienPrecedents.length});
+      if(data[j+1] != "/"){ //Si l'élément actuel est parent.
+        ancienPrecedents.push(precedent);
+        precedent = id;
+      }
+      id++;
+      text = false;
     }
-    console.log(mm);
-    for (var i = 0; i < mm.length; i++) {
-      $('#resultat').text($('#resultat').text()+mm[i].text);
-    }
-  });
-});
+  }
+  console.log(mm);
+}
